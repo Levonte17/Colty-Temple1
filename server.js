2f2a52433945d745acd589ce56556b77b3933778
Levonte Larry <levontelarry@Levontes-MacBook-Air.local>
2022-10-02 18:25:54 -0400

    app listening mongoose connected, indexroute setup

---
+++ server.js
@@ -0,0 +1,43 @@
+//DEPENDENCIES
+const express = require('express');
+const mongoose = require('mongoose');
+//const methodOverride = require('method-override');
+
+//INIT
+const app = express();
+
+//CONFIG
+require('dotenv').config();
+const PORT = process.env.PORT
+const DATABASE_URI = process.env.DATABASE_URI;
+const db = mongoose.connection;
+
+//CONNECT MONGODB
+mongoose.connect(DATABASE_URI);
+
+//MONGOOSE LISTENER
+db.on('connected', () => console.log('MongoDB Connected'));
+
+//MIDDLEWARE
+app.use(express.urlencoded({extended: false}));
+//app.use(methodOverride('_method'));
+
+//TODU
+////INDUCES////
+
+//HomePage//
+app.get('/',(req, res) => {
+res.render('index.ejs');
+});
+
+//PayerRequest//
+//Events&Services//
+//Community//
+//AboutUs//
+
+
+
+//LISTENER
+app.listen(PORT, () => {
+    console.log(`App Is Listening On Port ${PORT}`);
+});
